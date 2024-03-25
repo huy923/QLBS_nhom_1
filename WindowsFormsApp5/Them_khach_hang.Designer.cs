a994097b717abb8f81d599a80a1ca9451ab359dd
huy923 <huyxam233@gmail.com>
2024-03-25 20:50:32 +0700

    them khach hang

---
+++ WindowsFormsApp5/Them_khach_hang.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Them_khach_hang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Them_khach_hang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 729);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Them_khach_hang";
+            this.Text = "a";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
