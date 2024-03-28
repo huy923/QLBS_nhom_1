b33e7f2b303c6c01bcdcc021c886d6dd02836c69
huy923 <huyxam233@gmail.com>
2024-03-28 21:42:38 +0700

    new

--- WindowsFormsApp5/Sua_tac_gia.Designer.cs
+++ WindowsFormsApp5/Sua_tac_gia.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sua_tac_gia));
             this.ten = new System.Windows.Forms.TextBox();
             this.que = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -42,72 +43,62 @@
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.them = new System.Windows.Forms.Button();
             this.nam_sinh = new System.Windows.Forms.DateTimePicker();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.qLBSDataSet = new WindowsFormsApp5.QLBSDataSet();
             this.tacgiaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.qLBSDataSet = new WindowsFormsApp5.QLBSDataSet();
             this.tac_giaTableAdapter = new WindowsFormsApp5.QLBSDataSetTableAdapters.Tac_giaTableAdapter();
+            this.nam_mat = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.maTGDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tentacgiaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.quequanDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.namsinhDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nammatDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.nam_mat = new System.Windows.Forms.DateTimePicker();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.qLBSDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tacgiaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLBSDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // ten
             // 
-            this.ten.Location = new System.Drawing.Point(228, 150);
+            resources.ApplyResources(this.ten, "ten");
             this.ten.Name = "ten";
-            this.ten.Size = new System.Drawing.Size(339, 31);
-            this.ten.TabIndex = 0;
             // 
             // que
             // 
-            this.que.Location = new System.Drawing.Point(228, 207);
+            resources.ApplyResources(this.que, "que");
             this.que.Name = "que";
-            this.que.Size = new System.Drawing.Size(339, 31);
-            this.que.TabIndex = 3;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 150);
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 25);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Tên tác giả : ";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 344);
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 25);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Năm Mất : ";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 277);
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 25);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Năm Sinh : ";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(34, 207);
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(124, 25);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Quê quán : ";
             // 
             // menuStrip1
             // 
@@ -115,65 +106,88 @@
             this.thêmKháchHàngToolStripMenuItem,
             this.thêmTácGiảToolStripMenuItem,
             this.kiểmTraHóaĐơnToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1159, 24);
-            this.menuStrip1.TabIndex = 10;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // thêmKháchHàngToolStripMenuItem
             // 
             this.thêmKháchHàngToolStripMenuItem.Name = "thêmKháchHàngToolStripMenuItem";
-            this.thêmKháchHàngToolStripMenuItem.Size = new System.Drawing.Size(114, 20);
-            this.thêmKháchHàngToolStripMenuItem.Text = "Thêm khách hàng";
+            resources.ApplyResources(this.thêmKháchHàngToolStripMenuItem, "thêmKháchHàngToolStripMenuItem");
             // 
             // thêmTácGiảToolStripMenuItem
             // 
             this.thêmTácGiảToolStripMenuItem.Name = "thêmTácGiảToolStripMenuItem";
-            this.thêmTácGiảToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
-            this.thêmTácGiảToolStripMenuItem.Text = "Thêm tác giả";
+            resources.ApplyResources(this.thêmTácGiảToolStripMenuItem, "thêmTácGiảToolStripMenuItem");
             // 
             // kiểmTraHóaĐơnToolStripMenuItem
             // 
             this.kiểmTraHóaĐơnToolStripMenuItem.Name = "kiểmTraHóaĐơnToolStripMenuItem";
-            this.kiểmTraHóaĐơnToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
-            this.kiểmTraHóaĐơnToolStripMenuItem.Text = "Kiểm tra hóa đơn";
+            resources.ApplyResources(this.kiểmTraHóaĐơnToolStripMenuItem, "kiểmTraHóaĐơnToolStripMenuItem");
             // 
             // pictureBox5
             // 
             this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            resources.ApplyResources(this.pictureBox5, "pictureBox5");
             this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox5.Image = global::WindowsFormsApp5.Properties.Resources.button__2_;
-            this.pictureBox5.Location = new System.Drawing.Point(1109, 0);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(50, 48);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 11;
             this.pictureBox5.TabStop = false;
             this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
             // 
             // them
             // 
-            this.them.Location = new System.Drawing.Point(39, 489);
+            resources.ApplyResources(this.them, "them");
             this.them.Name = "them";
-            this.them.Size = new System.Drawing.Size(109, 60);
-            this.them.TabIndex = 12;
-            this.them.Text = "Thêm ";
             this.them.UseVisualStyleBackColor = true;
             this.them.Click += new System.EventHandler(this.them_Click);
             // 
             // nam_sinh
             // 
-            this.nam_sinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.nam_sinh.Location = new System.Drawing.Point(228, 271);
+            resources.ApplyResources(this.nam_sinh, "nam_sinh");
             this.nam_sinh.Name = "nam_sinh";
-            this.nam_sinh.Size = new System.Drawing.Size(339, 31);
-            this.nam_sinh.TabIndex = 14;
+            this.nam_sinh.Value = new System.DateTime(1990, 1, 1, 0, 0, 0, 0);
+            // 
+            // tacgiaBindingSource
+            // 
+            this.tacgiaBindingSource.DataMember = "Tac_gia";
+            this.tacgiaBindingSource.DataSource = this.qLBSDataSet;
+            // 
+            // qLBSDataSet
+            // 
+            this.qLBSDataSet.DataSetName = "QLBSDataSet";
+            this.qLBSDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tac_giaTableAdapter
+            // 
+            this.tac_giaTableAdapter.ClearBeforeFill = true;
+            // 
+            // nam_mat
+            // 
+            resources.ApplyResources(this.nam_mat, "nam_mat");
+            this.nam_mat.Name = "nam_mat";
+            this.nam_mat.Value = new System.DateTime(1990, 1, 1, 0, 0, 0, 0);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.maTGDataGridViewTextBoxColumn,
@@ -182,73 +196,61 @@
             this.namsinhDataGridViewTextBoxColumn,
             this.nammatDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.tacgiaBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(573, 66);
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(574, 402);
-            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // qLBSDataSet
-            // 
-            this.qLBSDataSet.DataSetName = "QLBSDataSet";
-            this.qLBSDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // tacgiaBindingSource
-            // 
-            this.tacgiaBindingSource.DataMember = "Tac_gia";
-            this.tacgiaBindingSource.DataSource = this.qLBSDataSet;
-            // 
-            // tac_giaTableAdapter
-            // 
-            this.tac_giaTableAdapter.ClearBeforeFill = true;
             // 
             // maTGDataGridViewTextBoxColumn
             // 
+            this.maTGDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.maTGDataGridViewTextBoxColumn.DataPropertyName = "MaTG";
-            this.maTGDataGridViewTextBoxColumn.HeaderText = "MaTG";
+            resources.ApplyResources(this.maTGDataGridViewTextBoxColumn, "maTGDataGridViewTextBoxColumn");
             this.maTGDataGridViewTextBoxColumn.Name = "maTGDataGridViewTextBoxColumn";
+            this.maTGDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // tentacgiaDataGridViewTextBoxColumn
             // 
+            this.tentacgiaDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.tentacgiaDataGridViewTextBoxColumn.DataPropertyName = "Ten_tac_gia";
-            this.tentacgiaDataGridViewTextBoxColumn.HeaderText = "Ten_tac_gia";
+            resources.ApplyResources(this.tentacgiaDataGridViewTextBoxColumn, "tentacgiaDataGridViewTextBoxColumn");
             this.tentacgiaDataGridViewTextBoxColumn.Name = "tentacgiaDataGridViewTextBoxColumn";
+            this.tentacgiaDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // quequanDataGridViewTextBoxColumn
             // 
+            this.quequanDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.quequanDataGridViewTextBoxColumn.DataPropertyName = "Que_quan";
-            this.quequanDataGridViewTextBoxColumn.HeaderText = "Que_quan";
+            resources.ApplyResources(this.quequanDataGridViewTextBoxColumn, "quequanDataGridViewTextBoxColumn");
             this.quequanDataGridViewTextBoxColumn.Name = "quequanDataGridViewTextBoxColumn";
+            this.quequanDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // namsinhDataGridViewTextBoxColumn
             // 
+            this.namsinhDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.namsinhDataGridViewTextBoxColumn.DataPropertyName = "Nam_sinh";
-            this.namsinhDataGridViewTextBoxColumn.HeaderText = "Nam_sinh";
+            resources.ApplyResources(this.namsinhDataGridViewTextBoxColumn, "namsinhDataGridViewTextBoxColumn");
             this.namsinhDataGridViewTextBoxColumn.Name = "namsinhDataGridViewTextBoxColumn";
+            this.namsinhDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // nammatDataGridViewTextBoxColumn
             // 
+            this.nammatDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.nammatDataGridViewTextBoxColumn.DataPropertyName = "Nam_mat";
-            this.nammatDataGridViewTextBoxColumn.HeaderText = "Nam_mat";
+            resources.ApplyResources(this.nammatDataGridViewTextBoxColumn, "nammatDataGridViewTextBoxColumn");
             this.nammatDataGridViewTextBoxColumn.Name = "nammatDataGridViewTextBoxColumn";
-            // 
-            // nam_mat
-            // 
-            this.nam_mat.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.nam_mat.Location = new System.Drawing.Point(228, 344);
-            this.nam_mat.Name = "nam_mat";
-            this.nam_mat.Size = new System.Drawing.Size(339, 31);
-            this.nam_mat.TabIndex = 17;
+            this.nammatDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // Sua_tac_gia
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowsFormsApp5.Properties.Resources._1026345;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1159, 657);
-            this.Controls.Add(this.nam_mat);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nam_mat);
             this.Controls.Add(this.nam_sinh);
             this.Controls.Add(this.them);
             this.Controls.Add(this.pictureBox5);
@@ -259,18 +261,15 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.que);
             this.Controls.Add(this.ten);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Sua_tac_gia";
-            this.Text = "Them_tac_gia";
             this.Load += new System.EventHandler(this.Them_tac_gia_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.qLBSDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tacgiaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qLBSDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -291,15 +290,17 @@
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.Button them;
         private System.Windows.Forms.DateTimePicker nam_sinh;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private QLBSDataSet qLBSDataSet;
         private System.Windows.Forms.BindingSource tacgiaBindingSource;
         private QLBSDataSetTableAdapters.Tac_giaTableAdapter tac_giaTableAdapter;
+        private System.Windows.Forms.DateTimePicker nam_mat;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn maTGDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn tentacgiaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn quequanDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn namsinhDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn nammatDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DateTimePicker nam_mat;
     }
 }
--- WindowsFormsApp5/Sua_tac_gia.cs
+++ WindowsFormsApp5/Sua_tac_gia.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Linq.Expressions;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 namespace WindowsFormsApp5
 {
     public partial class Sua_tac_gia : Form
@@ -43,17 +44,88 @@ namespace WindowsFormsApp5
         private void Them_tac_gia_Load(object sender, EventArgs e)
         {
             this.tac_giaTableAdapter.Fill(this.qLBSDataSet.Tac_gia);
-            //this.tac_giaTableAdapter.Update
         }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            
-        }
-
         private void pictureBox5_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+            string tenTacGia = selectedRow.Cells[1].Value?.ToString();
+            string quenha = selectedRow.Cells[2].Value?.ToString();
+            string namSinh = selectedRow.Cells[3].Value?.ToString();
+            string namMat = selectedRow.Cells[4].Value?.ToString();
+            ten.Text = tenTacGia ?? "";
+            que.Text = quenha ?? "";
+            DateTime namSinhDate = new DateTime();
+            if (DateTime.TryParse(namSinh, out namSinhDate))
+            {
+                nam_sinh.Value = namSinhDate;
+            }
+            DateTime namMatDate = new DateTime();
+            if (DateTime.TryParse(namMat, out namMatDate))
+            {
+                nam_mat.Value = namMatDate;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                string tenTacGia = ten.Text;
+                string quenQuan = que.Text;
+                DateTime namSinh = nam_sinh.Value;
+                DateTime namMat = nam_mat.Value;
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                string tacGiaID = selectedRow.Cells[0].Value.ToString(); 
+                Tac_gia tacGiaToUpdate = db.Tac_gia.FirstOrDefault(tg => tg.MaTG == tacGiaID);
+                if (tacGiaToUpdate != null)
+                {
+                    tacGiaToUpdate.Ten_tac_gia = tenTacGia;
+                    tacGiaToUpdate.Que_quan = quenQuan;
+                    tacGiaToUpdate.Nam_sinh = namSinh;
+                    tacGiaToUpdate.Nam_mat = namMat;
+                    db.SaveChanges();
+                    this.tac_giaTableAdapter.Fill(this.qLBSDataSet.Tac_gia);
+                    MessageBox.Show("Đã cập nhật thông tin tác giả thành công!");
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy tác giả cần sửa!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một tác giả để sửa!");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                string tacGiaID = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                Tac_gia tacGiaToDelete = db.Tac_gia.FirstOrDefault(tg => tg.MaTG == tacGiaID);
+                if (tacGiaToDelete != null)
+                {
+                    DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn xóa tác giả này?", "Xác nhận xóa", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        db.Tac_gia.Remove(tacGiaToDelete);
+                        db.SaveChanges();
+                        dataGridView1.Rows.Remove(dataGridView1.SelectedRows[0]);
+                        MessageBox.Show("Đã xóa tác giả thành công!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy tác giả cần xóa!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một tác giả để xóa!");
+            }
         }
     }
 }
